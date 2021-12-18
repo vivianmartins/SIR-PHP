ab2b813ef6456f1dd5cb52c9114445f43b787f3e
vivianmartins <64610367+vivianmartins@users.noreply.github.com>
2021-12-18 15:29:09 +0000

    navbar

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="estilos/navbar.css">
+	<link rel="shortcut icon" href="image/favi.png" />
+	<title>221B</title>
+</head>
+<body>
+
+<!-- FALTA A GRID BOOTSTRAP, DROPDOWN E COLOCAR A FUNCIONAR -->
+<ul>
+  <li>   <img src="image/4_1.png" class="imag" alt="logo"></li>
+  <li><a href="#home">Início</a></li>
+  <li><a href="#filtrae">Filtrar</a></li>
+  <li><a href="#criar">Criar</a></li>
+  <!--<li> <input type="text" style="float:right " class="pesquisar" placeholder="Pesquisar"></li> --> 
+  <li style="float:right "><a class="active" href="#about">Perfil</a></li>
+</ul>
+</body>
+
+ 
+	<!-- fazer navbar, footer e lista de cards -->
+
+	
