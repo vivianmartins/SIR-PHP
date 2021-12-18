d4f97eb523a5bb8bd34a1ec8bce43d401e9f7c96
vivianmartins <64610367+vivianmartins@users.noreply.github.com>
2021-12-18 15:27:12 +0000

    navbar

--- navbar.php
+++ navbar.php
@@ -1,26 +1,25 @@
 <nav>
-	<ul>
-		<li class="nav-item">
+	<ul class="menu"  >
+		<li >
 			<img src="image/4_1.png" class="imag" alt="logo">
 		</li>
-		<li class="nav-item">
-			<a href="#"> Filtrar </a>
+		<li><a href="#"> Filtrar </a></li>
+		<li ><a href="#">Criar</a>
+			<ul>
+			<l ><a href="#">Tipos</a></l>
+			<l><a href="#">Categorias</a></l>
+			<l><a href="#">Apontamentos</a></l>
+			</ul>
 		</li>
-		<li class="nav-item dropdown">
-			<a class="dropdown-btn" href="#"> Criar </a>
-			<div class="dropdown-content">
-				<p>Hello World!</p>
-				<p>Hello World!</p>
-				<p>Hello World!</p>
-				<p>Hello World!</p>
-				<p>Hello World!</p>
-				<p>Hello World!</p>
-				<p>Hello World!</p>
-			</div>
-		</li>
+		<!--SUBMENU DO USERNAME-->
 		<!--<li> <input type="text" style="float:right " class="pesquisar" placeholder="Pesquisar"></li> -->
-		<li class="nav-item" style="float:right ">
+		<li  style="float:right ">
 			<a href="#about"> Username </a>
+			<ul>
+			<l1 ><a href="#">Perfil</a></l1>
+			<l1><a href="#">Logout</a></l1>
+		
+			</ul>
 		</li>
 	</ul>
 </nav>
