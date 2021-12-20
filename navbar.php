76e1729303f127d0b9b33c9984294d89942b5dd5
vivianmartins <64610367+vivianmartins@users.noreply.github.com>
2021-12-20 13:16:58 +0000

    navbar

--- navbar.php
+++ navbar.php
@@ -3,9 +3,12 @@
 		<li >
 			<img src="./image/4_1.png" class="imag" alt="logo">
 		</li>
+			
+		<li><a style="	font-size: 22px;" href="./home.php"> Início </a></li>
 		<li><a style="	font-size: 22px;" href="#"> Filtrar </a></li>
 		<li ><a style="	font-size: 22px;" href="#">Criar</a>
 			<ul>
+		
 			<l ><a href="./tipos.php">Tipos</a></l>
 			<!-- <l><a href="#">Categorias</a></l> -->
 			<l><a href="#">Apontamentos</a></l>
@@ -17,7 +20,7 @@
 			<a  style="	font-size: 22px;"href="#about"> Username </a>
 			<ul>
 			<l1 ><a href="#">Perfil</a></l1>
-			<l1><a href="logout.php">Logout</a></l1>
+			<l1><a href="./logout.php">Logout</a></l1>
 		
 			</ul>
 		</li>
