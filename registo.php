25aa311a87d645e5b5185cc25283b95afd248bf4
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2021-12-26 22:24:10 +0000

    bug-fix
    Registo não permite emails duplicados
    Desativar conta desativa a conta

--- registo.php
+++ registo.php
@@ -10,15 +10,27 @@ if(isset($_POST['registar'])){
     $hashed_senha = password_hash( $senha, PASSWORD_DEFAULT);
 
     try{
-        $SQLInsert = "INSERT INTO utilizadores (nome, email, senha)
-        values (:nome,  :email, :senha)";
+        // Procura por um registo com o mesmo email
+        $st = $pdo->prepare('SELECT * FROM utilizadores WHERE email = :email');
+        $st->bindValue(':email', $email);
+        $st->execute();
+        $res = $st->fetchAll(PDO::FETCH_ASSOC);
 
-        $statement = $pdo -> prepare ($SQLInsert);
-        $statement -> execute (array(':nome' => $nome, ':email' => $email, ':senha' => $hashed_senha ));
+        // Se nao existir um registo com o mesmo email cria um novo registo
+        if(empty($res)) {
+            $SQLInsert = "INSERT INTO utilizadores (nome, email, senha)
+            values (:nome,  :email, :senha)";
 
-        if($statement-> rowCount() == 1) {
-            $result = header('location: index.php');
+            $statement = $pdo -> prepare ($SQLInsert);
+            $statement -> execute (array(':nome' => $nome, ':email' => $email, ':senha' => $hashed_senha ));
 
+            if($statement-> rowCount() == 1) {
+                $result = header('location: index.php');
+
+            }
+        } else {
+            // colocar aqui os erros
+            header('location: registar.php');
         }
     }catch(PDOException $e){
             echo "erro: " .$e->getMessage();
